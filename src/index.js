06eef1654c7c9aff1dc0ff4f1d247f1b36326633
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-04-22 20:33:14 +0300

    footer

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const openModalBtn = document.getElementById('open-modal');
+const closeModalBtn = document.getElementById('close-modal');
+const modal = document.getElementById('modal');
+
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', event => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
