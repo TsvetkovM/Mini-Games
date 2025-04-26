8c9b52f50f9c6afc6f2792a1de4f53aec654b5fb
DemianPorada <129092912+DemianPorada@users.noreply.github.com>
2025-04-26 19:13:21 +0300

    /2

--- src/partials/js/subscribtion-modal.js
+++ src/partials/js/subscribtion-modal.js
@@ -1,18 +1,12 @@
-    const modal = document.getElementById("subscriptionModal");
-    const openBtn = document.getElementById("subscribeBtn");
-    const closeBtn = document.getElementById("closeModal");
+const openModalBtn = document.querySelector('.open-modal-btn');
+const closeModalBtn = document.querySelector('.close-modal-btn');
+const modal = document.querySelector('.modal');
 
-    openBtn.addEventListener("click", () => {
-      modal.style.display = "flex";
-    });
+openModalBtn.addEventListener('click', () => {
+  modal.classList.add('is-open');
+});
 
-    closeBtn.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
-
-    window.addEventListener("click", (e) => {
-      if (e.target === modal) {
-        modal.style.display = "none";
-      }
-    });
+closeModalBtn.addEventListener('click', () => {
+  modal.classList.remove('is-open');
+});
  
